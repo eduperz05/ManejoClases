0055e5172cc3241ca193ffa6d33d788869475404
HolaMundoJava <eduperz38@gmail.com>
2023-04-20 10:25:20 +0100

    Caja contiene + Alumno

---
+++ src/main/java/com/cajas/Caja.java
@@ -0,0 +1,16 @@
+package com.cajas;
+
+public class Caja {
+
+    private double peso;
+
+    public Caja(double peso) {
+        this.peso = peso;
+    }
+
+
+    public double getPeso() {
+        return peso;
+    }
+
+}
