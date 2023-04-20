f1b32defa8e320cef9a4a1d3b611d36b2e664688
HolaMundoJava <eduperz38@gmail.com>
2023-04-20 09:22:38 +0100

    Cajas + notas

---
+++ src/main/java/com/cajas/Cajas.java
@@ -0,0 +1,14 @@
+package com.cajas;
+
+public class Cajas {
+    private double peso;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.notas.Nota;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Nota n1 = new Nota(6);
+        System.out.println("n1.getQualification() = " + n1.getQualification());
+    }
+}
